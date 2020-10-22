668676850de00ee1db2cba601cd1947be5d37166
Winston Isaac <marcwinstonisaac@gmail.com>
2020-10-23 00:52:32 +0800

    can now check columns

--- EZSudoku.js
+++ EZSudoku.js
@@ -4,34 +4,53 @@ var board = [
     [0, 0, 2, 0],
     [2, 1, 0, 0]
 ]
-var possibleEntries = [];
-var positionIndex = 0;
-for (let x = 0; x < Math.pow(board.length,2); x++) {
-    possibleEntries[x] = new Array();
-}
-for (let x = 0; x < board.length; x++){
-    for (let y = 0; y<board[x].length; y++){
-        if (board[x][y]){
-            possibleEntries[positionIndex][0] = board[x][y];
+function solver (board){
+    var positionIndex = 0;
+    var possibleEntries = [];
+    for (let x = 0; x < Math.pow(board.length,2); x++) {
+        possibleEntries[x] = new Array();
+    }
+    for (let x = 0; x < board.length; x++){
+        for (let y = 0; y<board[x].length; y++){
+            if (board[x][y]){
+                possibleEntries[positionIndex][0] = board[x][y];
+                positionIndex ++;
+                continue;
+        }else{
+                for (let z = 0; z < board.length; z++){
+                possibleEntries[positionIndex][z] = z + 1;
+                }
+                for (let sameRow = 0; sameRow <board.length; sameRow++){
+                    if (board[x][sameRow]){
+                        if (possibleEntries[positionIndex].length === 1){
+                            continue;
+                        }
+                        if (possibleEntries[positionIndex].includes(board[x][sameRow])){
+                            possibleEntries[positionIndex].splice(possibleEntries[positionIndex].indexOf(board[x][sameRow]), 1);
+                        }
+                    }
+                }
+                for (let sameColumn = 0; sameColumn <board.length; sameColumn++){
+                    if (board[sameColumn][y]){
+                        if (possibleEntries[positionIndex].length === 1){
+                            continue;
+                        }
+                        if (possibleEntries[positionIndex].includes(board[sameColumn][y])){
+                            possibleEntries[positionIndex].splice(possibleEntries[positionIndex].indexOf(board[sameColumn][y]), 1);
+                        }
+                    }
+                }
+            }
             positionIndex ++;
-            continue;
-       }else{
-            for (let z = 0; z < board.length; z++){
-               possibleEntries[positionIndex][z] = z + 1;
-            }
-            for (let sameRow = 0; sameRow <board.length; sameRow++){
-                if (board[x][sameRow]){
-                    let elim = possibleEntries[positionIndex].indexOf(board[x][sameRow]);
-                    possibleEntries[positionIndex].splice(elim, 1);
-                }
-            }
-            for (let sameColumn = 0; sameColumn <board.length; sameColumn++){
-                if (board[sameColumn][y]){
-                    let elim = possibleEntries[positionIndex].indexOf(board[sameColumn][y]);
-                    possibleEntries[positionIndex].splice(elim, 1);
-                }
-            }
+        }    
+    }
+    for (let a = 0; a < Math.pow(board.length,2); a++){
+        if (possibleEntries[a].length === 1){
+            board[Math.floor(a / board.length)][a % board.length] = possibleEntries[a][0];
         }
-        positionIndex ++;
-    }    
+    }
 }
+solver(board);
+console.log(board);
+solver(board);
+console.log(board);
