ef86325040973d340a3f76f84694d77e1d7fd5ff
Winston Isaac <marcwinstonisaac@gmail.com>
2020-10-31 19:12:41 +0800

    started GUI for mobiles, no functionality yet

--- assets/EZSudoku.js
+++ assets/EZSudoku.js
@@ -1,39 +1,3 @@
-/*var board = [
-    [3, 0, 0, 2],
-    [0, 0, 3, 0],
-    [4, 0, 2, 0],
-    [2, 0, 0, 3]
-]*/
-/*var board = [
-    [1, 0, 0, 0],
-    [3, 2, 0, 4],
-    [2, 0, 0, 1],
-    [0, 0, 3, 0]
-]*/
-const {performance} = require('perf_hooks');
-var t0 = performance.now()
-/*var board = [
-    [0, 0, 0, 2, 6, 0, 7, 0, 1],
-    [6, 8, 0, 0, 7, 0, 0, 9, 0],
-    [1, 9, 0, 0, 0, 4, 5, 0, 0],
-    [8, 2, 0, 1, 0, 0, 0, 4, 0],
-    [0, 0, 4, 6, 0, 2, 9, 0, 0],
-    [0, 5, 0, 0, 0, 3, 0, 2, 8],
-    [0, 0, 9, 3, 0, 0, 0, 7, 4],
-    [0, 4, 0, 0, 5, 0 ,0, 3, 6],
-    [7, 0, 3, 0, 1, 8, 0, 0, 0]
-]*/
-var board = [   //hard 1
-    [0, 0, 0, 0, 0, 0, 0, 0, 2],
-    [0, 0, 0, 0, 0, 0, 9, 4, 0],
-    [0, 0, 3, 0, 0, 0, 0, 0, 5],
-    [0, 9, 2, 3, 0, 5, 0, 7, 4],
-    [8, 4, 0, 0, 0, 0, 0, 0, 0],
-    [0, 6, 7, 0, 9, 8, 0, 0, 0],
-    [0, 0, 0, 7, 0, 6, 0, 0, 0],
-    [0, 0, 0, 9, 0, 0 ,0, 2, 0],
-    [4, 0, 8, 5, 0, 0, 3, 6, 0]
-]
 var board = [   //hard 2
     [0, 7, 0, 0, 1, 0, 0, 2, 0],
     [5, 0, 0, 0, 2, 7, 0, 0, 1],
@@ -45,23 +9,6 @@ var board = [   //hard 2
     [0, 2, 0, 7, 5, 0 ,0, 0, 0],
     [0, 0, 0, 2, 3, 0, 4, 8, 0]
 ]
-/*var board = [   //demonly headteacher 1
-    [0, 0, 7, 3, 0, 0, 2, 0, 5],
-    [0, 0, 4, 0, 0, 9, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 7, 6, 0],
-    [4, 0, 0, 0, 0, 0, 0, 7, 8],
-    [0, 1, 0, 5, 0, 0, 0, 0, 2],
-    [0, 0, 8, 0, 0, 6, 1, 0, 0],
-    [0, 2, 0, 0, 0, 1, 4, 0, 0],
-    [0, 0, 0, 8, 0, 0 ,0, 3, 7],
-    [0, 0, 0, 0, 0, 5, 0, 0, 0]
-]*/
-/*var board = [
-    [0, 0, 4, 0],
-    [1, 0, 0, 0],
-    [0, 2, 0, 0],
-    [0, 0, 0, 3]
-]*/
 function subtractTwoArrays(arr1,arr2){
     for(x = 0; x < arr2.length; x++){
         if(arr1.includes(arr2[x])){
@@ -90,11 +37,9 @@ for (let x = 0; x < board.length; x++){         //for loop for the rows
     }
 }
 function solver (board){
-    //console.log(possibleEntries[24]);
     var positionIndex = 0;
     for (let x = 0; x < board.length; x++){         //for loop for the rows
         for (let y = 0; y< board.length; y++){      //for loop for the columns
-            //console.log('pos ' + positionIndex);
             if (board[x][y]){                       //goes here if cell is a given
                 possibleEntries[positionIndex][0] = board[x][y];
                 positionIndex ++;
@@ -176,15 +121,16 @@ function solver (board){
                         possibleEntries[positionIndex] = possibleValues;
                     }
                 }
-            positionIndex ++;
-        }    
+                positionIndex ++;
+            }    
+        }
+        for (let a = 0; a < Math.pow(board.length,2); a++){
+            if (possibleEntries[a].length === 1){
+                board[Math.floor(a / board.length)][a % board.length] = possibleEntries[a][0];
+            }
+        }
     }
-    for (let a = 0; a < Math.pow(board.length,2); a++){
-        if (possibleEntries[a].length === 1){
-            board[Math.floor(a / board.length)][a % board.length] = possibleEntries[a][0];
-       }
-    }
-}}
+}
 var quadrants = new Array (board.length);
 for (let x = 0, points = 0; x < board.length; x++){         //group cell indexes that belong to each quadrant
     quadrants[x] = new Array (board.length);
@@ -216,13 +162,13 @@ var brute = false;
 while (stillIncomplete(board, 0)){
     solver(board);
     timeout ++;
-    if(timeout === 100){
+    if(timeout === 30){
         solverFallback(board);
         brute = true;
         break;
     }
 }
-function isValid(board, row, col, k) {
+function isValid(board, row, col, k) {      // this is the backtracking method by M. Ramezani of Stack Overflow
     for (let i = 0; i < 9; i++) {
         const m = 3 * Math.floor(row / 3) + Math.floor(i / 3);
         const n = 3 * Math.floor(col / 3) + i % 3;
@@ -232,30 +178,28 @@ function isValid(board, row, col, k) {
     }
     return true;
 }
-function solverFallback(data) {
-  for (let i = 0; i < 9; i++) {
-    for (let j = 0; j < 9; j++) {
-      if (data[i][j] == 0) {
-        for (let k = 1; k <= 9; k++) {
-          if (isValid(data, i, j, k)) {
-            data[i][j] = k;
-          if (solverFallback(data)) {
-           return true;
-          } else {
-           data[i][j] = 0;
-          }
-         }
-       }
-       return false;
-     }
-   }
- }
- return true;
-}
+function solverFallback(board) {
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            if (board[i][j] == 0) {
+                for (let k = 1; k <= 9; k++) {
+                    if (isValid(board, i, j, k)) {
+                        board[i][j] = k;
+                        if (solverFallback(board)) {
+                            return true;
+                        }else{
+                            board[i][j] = 0;
+                        }
+                    }
+                }
+            return false;
+            }  
+        }
+    }
+return true;
+}       // backtracking method ends here
 console.log('Solution:');
 console.log(board);
-var t1 = performance.now();
-console.log("That took " + (t1 - t0) + " milliseconds and " + timeout + ' tries.');
 if (brute){
     console.log("Solved using backtracking.")
 }
